e69d525df0ed18a6728178ed6c061f80fc9ae1b5
Matt <matt@thinkmade.com>
2021-02-10 10:55:24 +0100

    Update isr.js

--- pages/isr.js
+++ pages/isr.js
@@ -2,6 +2,7 @@ import Page from '../Page';
 import { getRandomAPI } from '../publicApis';
 
 //https://github.com/vercel/next.js/issues/5354
+// https://www.johanbleuzen.fr/blog/next-remove-clientside-javascript
 
 export default Page;
 
